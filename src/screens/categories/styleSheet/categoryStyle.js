64e68b616d3312410cdd0ba0b67fe223b67d16ee
Youssef Ashraf <142300609+youssef604@users.noreply.github.com>
2024-05-10 20:13:40 +0300

    ..

--- src/screens/categories/styleSheet/categoryStyle.js
+++ src/screens/categories/styleSheet/categoryStyle.js
@@ -2,27 +2,37 @@ import { StyleSheet } from 'react-native';
 
 const categoryStyle = StyleSheet.create({
     inputContainer: {
-        flex: 1 ,
-        flexDirection : 'row',
-        alignItems : 'center',
-        justifyContent: 'space-between',
-        marginVertical: 20,
-        backgroundColor: '#e5f0f7',
-        padding: '1%',
-        borderRadius: 25,
+        flex: 1,
+        backgroundColor: '#f5f7fa',
+
+    },
+    header: {
+        backgroundColor: '#001b46',
+        paddingVertical: 20,
+        borderBottomStartRadius: 15,
+        borderBottomEndRadius: 15,
+        alignItems: 'center',
+    },
+    box: {
+        flexDirection: 'row',
+        backgroundColor: 'white',
+        width: '90%', borderRadius: 5
         
     },
-    img: {
-        width: 75,
-        height: 75,
-        
+    input: {
+        flex: 1,
+        height: 40,
+        borderColor: 'white',
+        borderWidth: 1,
+        borderRadius: 5,
+        marginRight: 10,
+        padding: 10,
+
     },
-    text: {
-        fontWeight: "bold",
-        fontSize: 15,
-        margin: 5,
-        padding : 5
-        
+    list: {
+        flex: 1,
+        padding: '5%', 
+    
     }
 })
 
